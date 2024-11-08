cf6d3e516bec07b945e8796650c54c76a22e189c
Megan Sioco <megan_rylene_sioco@dlsu.edu.ph>
2024-11-08 12:48:16 +0800

    Implement register

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -4,33 +4,30 @@ const bcrypt = require('bcrypt');
 
 async function uploadUser(req, res) {
     const username = req.body.username;
+    const contact = req.body.contact;
+    const restaurantName = req.body.restaurant;
     const email = req.body.email;
     const password = req.body.password;
     const hashedPassword =  await bcrypt.hash(password, 10);
     const pfp = req.file;
-    const exists = await User.find({ email: email});
+    const exists = await User.find({ email: email });
+    const accType = "Customer"; // Only Sustainabowl can directly employe Sales and Logistics staff
 
     if(exists.length === 0) {
         //Determine what the last userID is
         const numUsers = await User.countDocuments();
         const newUserID = 10001 + parseInt(numUsers);
 
-        //Determine which account type is selected
-        var selectedRadio = req.body['account-type'];
-        if (selectedRadio == "Lab Technician") {
-            accType = "Lab Technician";
-        } else {
-            accType = "Student";
-        }
-
         try {
             User.create({
                 userID: newUserID.toString(),
                 name: username,
-                accType: accType,
                 email: email,
                 password: hashedPassword,
-                pfp: "/profile-pictures/" + pfp.originalname
+                phone: "+" + contact.toString(),
+                usertype: accType,
+                restaurantName: restaurantName,
+                // pfp: "/profile-pictures/" + pfp.originalname
             });
 
             if(User.findOne({ userID: newUserID })) {
