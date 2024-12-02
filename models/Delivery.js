3bf4f42e7c0f8aa13a5a254965427c292dceabb8
Megan Sioco <megan_rylene_sioco@dlsu.edu.ph>
2024-12-03 00:34:32 +0800

    Minor edits to delivery schema

--- models/Delivery.js
+++ models/Delivery.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const DeliverySchema = new mongoose.Schema({
-    deliveryID: { type: String }, // 70001
-    orderID: { type: String }, // 40001
+    deliveryID: { type: Number }, // 70001
+    orderID: { type: Number }, // 40001
     isPaid: { type: Boolean, default: false },
     deliveredOn: { type: String }, 
 });
